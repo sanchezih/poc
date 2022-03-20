438e22a4a86e566442cfa5bdcc4718da2a9a2056
IHS-WORKSTATION\ihsanch <ignacio.sanchez@comafi.com.ar>
2022-03-20 17:31:06 -0300

    fix

--- poc-springboot-security/src/main/java/com/github/sanchezih/springsec/security/jwt/JwtUtils.java
+++ poc-springboot-security/src/main/java/com/github/sanchezih/springsec/security/jwt/JwtUtils.java
@@ -14,6 +14,7 @@ import io.jsonwebtoken.*;
 
 @Component
 public class JwtUtils {
+	
 	private static final Logger logger = LoggerFactory.getLogger(JwtUtils.class);
 
 	@Value("${bezkoder.app.jwtSecret}")
