06a0ca1692f98de838732e784e088e2e46759e62
IHS-WORKSTATION\ihsanch <ignacio.sanchez@comafi.com.ar>
2022-05-22 08:33:11 -0300

    add swagger (poc spring sec con jwt)

--- poc-spring-security/poc-ss-03-jwt-auth/src/main/java/com/bezkoder/springjwt/config/SwaggerConfig.java
+++ poc-spring-security/poc-ss-03-jwt-auth/src/main/java/com/bezkoder/springjwt/config/SwaggerConfig.java
@@ -1,18 +1,17 @@
 package com.bezkoder.springjwt.config;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.ApiKey;
 import springfox.documentation.service.AuthorizationScope;
-import springfox.documentation.service.Contact;
 import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spi.service.contexts.SecurityContext;
@@ -21,42 +20,35 @@ import springfox.documentation.spring.web.plugins.Docket;
 @Configuration
 public class SwaggerConfig {
 
-	public static final String AUTHORIZATION_HEADER = "Authorization";
-
-	private final static String BASE_PACKAGE = "com.github.sanchezih.aerodevs.controller";
+	private final static String BASE_PACKAGE = "com.bezkoder.springjwt";
 
 	@Bean
 	public Docket api() {
-		return new Docket(DocumentationType.SWAGGER_2).apiInfo(getApiInfo())
+		return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo())
 				.securityContexts(Arrays.asList(securityContext())).securitySchemes(Arrays.asList(apiKey())).select()
 				.apis(RequestHandlerSelectors.basePackage(BASE_PACKAGE)).paths(PathSelectors.any()).build();
 	}
 
-	private ApiInfo getApiInfo() {
-		return new ApiInfo("Aerodevs API", // titulo
-				"Una descripcion...", // descripcion
-				"1.0", // version
-				"http://aerodevs.com.ar/terms", // termsOfServiceUrl
-				new Contact("Aerodevs", "http://aerodevs.com.ar", "info@aerodevs.com.ar"), // contact
-				"LICENSE", // license
-				"LICENSE URL", // licenseUrl
-				Collections.emptyList() // vendorExtensions
-		);
-	}
-
-	private ApiKey apiKey() {
-		return new ApiKey("JWT", AUTHORIZATION_HEADER, "header");
+	private ApiInfo apiInfo() {
+		return new ApiInfoBuilder() //
+				.title("Spring Boot REST API") //
+				.version("1.0.0") //
+				.description("Spring Boot REST API desc...") //
+				.build();
 	}
 
 	private SecurityContext securityContext() {
 		return SecurityContext.builder().securityReferences(defaultAuth()).build();
 	}
 
-	List<SecurityReference> defaultAuth() {
+	private List<SecurityReference> defaultAuth() {
 		AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
 		AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
 		authorizationScopes[0] = authorizationScope;
 		return Arrays.asList(new SecurityReference("JWT", authorizationScopes));
 	}
 
+	private ApiKey apiKey() {
+		return new ApiKey("JWT", "Authorization", "header");
+	}
 }
--- poc-spring-security/poc-ss-03-jwt-auth/src/main/java/com/bezkoder/springjwt/security/WebSecurityConfig.java
+++ poc-spring-security/poc-ss-03-jwt-auth/src/main/java/com/bezkoder/springjwt/security/WebSecurityConfig.java
@@ -1,4 +1,5 @@
 package com.bezkoder.springjwt.security;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -6,6 +7,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -18,50 +20,64 @@ import com.bezkoder.springjwt.security.services.UserDetailsServiceImpl;
 
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(
-		// securedEnabled = true,
-		// jsr250Enabled = true,
-		prePostEnabled = true)
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-	
+
+	private static final String[] AUTH_WHITELIST = {
+			// -- Swagger UI v2
+			"/v2/api-docs", //
+			"/swagger-resources", //
+			"/swagger-resources/**", //
+			"/configuration/ui", //
+			"/configuration/security", //
+			"/swagger-ui.html", //
+			"/webjars/**", //
+			// -- Swagger UI v3 (OpenAPI)
+			"/v3/api-docs/**", //
+			"/swagger-ui/**", //
+			// other public endpoints of your API may be appended to this array
+			"/api/auth/**", //
+			"/api/test/**" };
+
 	@Autowired
 	UserDetailsServiceImpl userDetailsService;
-	
+
 	@Autowired
 	private AuthEntryPointJwt unauthorizedHandler;
-	
+
 	@Bean
 	public AuthTokenFilter authenticationJwtTokenFilter() {
 		return new AuthTokenFilter();
 	}
-	
+
 	@Override
 	public void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
 		authenticationManagerBuilder.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
 	}
-	
+
 	@Bean
 	@Override
 	public AuthenticationManager authenticationManagerBean() throws Exception {
 		return super.authenticationManagerBean();
 	}
-	
+
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
-	
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.cors().and().csrf().disable()
-			.exceptionHandling()
-			.authenticationEntryPoint(unauthorizedHandler)
-			.and()
-			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-			.and()
-			.authorizeRequests().antMatchers("/api/auth/**").permitAll()
-			.antMatchers("/api/test/**").permitAll()
-			.anyRequest().authenticated();
+		http.cors().and().csrf().disable() //
+				.exceptionHandling()//
+				.authenticationEntryPoint(unauthorizedHandler)//
+				.and()//
+				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)//
+				.and()//
+				.authorizeRequests()//
+				.antMatchers(AUTH_WHITELIST).permitAll() // whitelist Swagger UI resources
+				.anyRequest().authenticated();//
 		http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
 	}
+
 }
