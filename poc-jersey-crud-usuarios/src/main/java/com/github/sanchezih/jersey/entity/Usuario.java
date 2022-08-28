c682cc30b2af1a51d40ba13d1ee2f8ac2693e91a
Ignacio Sanchez <sanchezih@gmail.com>
2022-08-28 17:03:45 -0300

    fixear

--- poc-jersey-crud-usuarios/src/main/java/com/github/sanchezih/jersey/entity/Usuario.java
+++ poc-jersey-crud-usuarios/src/main/java/com/github/sanchezih/jersey/entity/Usuario.java
@@ -5,24 +5,12 @@ import java.io.Serializable;
 public class Usuario implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	public String name;
 	public String username;
+	public String name;
 
-	public Usuario() {
+	public Usuario(String username, String name) {
 		super();
-	}
-
-	public Usuario(String name, String username) {
-		super();
-		this.name = name;
 		this.username = username;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
 		this.name = name;
 	}
 
@@ -34,4 +22,11 @@ public class Usuario implements Serializable {
 		this.username = username;
 	}
 
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 }
--- poc-jersey-crud-usuarios/src/main/java/com/github/sanchezih/jersey/service/UsuarioService.java
+++ poc-jersey-crud-usuarios/src/main/java/com/github/sanchezih/jersey/service/UsuarioService.java
@@ -23,9 +23,9 @@ public class UsuarioService {
 	private static List<Usuario> listaUsuarios = new ArrayList<Usuario>() {
 		private static final long serialVersionUID = 1L;
 		{
-			add(new Usuario("Rosa", "Marfil"));
-			add(new Usuario("Pepito", "Grillo"));
-			add(new Usuario("Manuela", "Lago"));
+			add(new Usuario("mperez", "Mariela"));
+			add(new Usuario("jlopez", "Juan"));
+			add(new Usuario("savila", "Sol"));
 		}
 	};
 
@@ -37,92 +37,88 @@ public class UsuarioService {
 
 	/**
 	 * 
-	 * @param name
+	 * @param username
 	 * @return
 	 */
 	@GET
 	@Path("/{name}")
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
-	public Response getUserById(@PathParam("name") String name) {
+	public Response getUserByUsername(@PathParam("username") String name) {
 		Usuario found = null;
-		for (int i = 0; i < listaUsuarios.size(); i++) {
-			if (listaUsuarios.get(i).getName().equalsIgnoreCase(name)) {
+		for (int i = 1; i < listaUsuarios.size(); i++) {
+			if (listaUsuarios.get(i).getUsername().equalsIgnoreCase(name)) {
 				found = listaUsuarios.get(i);
 			}
 		}
 		if (found == null) {
-			return Response.status(Status.BAD_REQUEST).entity("User not found").build();
+			return Response.status(Status.BAD_REQUEST).entity("Usuario no encontrado").build();
 		} else {
 			return Response.ok(found).build();
 		}
 	}
 
 	/**
-	 * NOTA: Si no existe el constructor vac�o de User, da un error y el userRequest
-	 * viene null.
+	 * NOTA: Debe existir el constructor por default en la entidad
 	 * 
 	 * @param userRequest
 	 * @return
 	 */
 	@POST
-	@Path("/createUser")
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response createUser(Usuario userRequest) {
 
-		this.listaUsuarios.add(userRequest);
+		this.listaUsuarios.add(null);
 		// return Response.status(Status.CREATED).build();
 		return Response.ok(listaUsuarios).build();
 
 	}
 
 	/**
-	 * NOTA: Si no existe el constructor vac�o de User, da un error y el userRequest
-	 * viene null.
+	 * NOTA: Debe existir el constructor por default en la entidad
 	 * 
-	 * @param userUpdate
+	 * @param usuario
 	 * @return
 	 */
 	@PUT
-	@Path("/updateUser")
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
-	public Response updateUser(Usuario userUpdate) {
+	public Response updateUser(Usuario usuario) {
 		Usuario found = null;
 		for (int i = 0; i < listaUsuarios.size(); i++) {
-			if (listaUsuarios.get(i).getName().equalsIgnoreCase(userUpdate.getName())) {
+			if (listaUsuarios.get(i).getName().equalsIgnoreCase(usuario.getUsername())) {
 				found = listaUsuarios.get(i);
 			}
 		}
 
 		if (found == null) {
-			return Response.status(Status.BAD_REQUEST).entity("User not found").build();
+			return Response.status(Status.BAD_REQUEST).entity("Usuario no encontrado").build();
 		} else {
-			found.setUsername(userUpdate.getUsername());
+			found.setName(usuario.getUsername());
 			return Response.ok(found).build();
 		}
 	}
 
 	/**
 	 * 
-	 * @param name
+	 * @param username
 	 * @return
 	 */
 	@DELETE
-	@Path("/deleteUser/{name}")
+	@Path("/{username}")
 	@Produces(MediaType.APPLICATION_JSON)
-	public Response deleteUser(@PathParam("name") String name) {
+	public Response deleteUser(@PathParam("username") String username) {
 		Usuario found = null;
 		for (int i = 0; i < listaUsuarios.size(); i++) {
-			if (listaUsuarios.get(i).getName().equalsIgnoreCase(name)) {
+			if (listaUsuarios.get(i).getName().equalsIgnoreCase(username)) {
 				found = listaUsuarios.get(i);
 				listaUsuarios.remove(found);
 			}
 		}
 
 		if (found == null) {
-			return Response.status(Status.BAD_REQUEST).entity("User not found").build();
+			return Response.status(Status.BAD_REQUEST).entity("Usuario no encontrado").build();
 		} else {
 			return Response.ok(listaUsuarios).build();
 		}
