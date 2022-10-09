e6c6ad08cf70c0a154da78b191d689ad26979eca
Ignacio Sanchez <ignacio.sanchez@comafi.com.ar>
2022-10-08 23:57:43 -0300

    refactor jwt based auth example

---
+++ poc-spring-security/02-jwt-based-auth/poc-ss-jwt-based-auth-01/src/main/java/com/bezkoder/springjwt/config/SwaggerConfig.java
@@ -0,0 +1,54 @@
+//package com.bezkoder.springjwt.config;
+//
+//import java.util.Arrays;
+//import java.util.List;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//import springfox.documentation.builders.ApiInfoBuilder;
+//import springfox.documentation.builders.PathSelectors;
+//import springfox.documentation.builders.RequestHandlerSelectors;
+//import springfox.documentation.service.ApiInfo;
+//import springfox.documentation.service.ApiKey;
+//import springfox.documentation.service.AuthorizationScope;
+//import springfox.documentation.service.SecurityReference;
+//import springfox.documentation.spi.DocumentationType;
+//import springfox.documentation.spi.service.contexts.SecurityContext;
+//import springfox.documentation.spring.web.plugins.Docket;
+//
+//@Configuration
+//public class SwaggerConfig {
+//
+//	private final static String BASE_PACKAGE = "com.bezkoder.springjwt";
+//
+//	@Bean
+//	public Docket api() {
+//		return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo())
+//				.securityContexts(Arrays.asList(securityContext())).securitySchemes(Arrays.asList(apiKey())).select()
+//				.apis(RequestHandlerSelectors.basePackage(BASE_PACKAGE)).paths(PathSelectors.any()).build();
+//	}
+//
+//	private ApiInfo apiInfo() {
+//		return new ApiInfoBuilder() //
+//				.title("Spring Boot REST API") //
+//				.version("1.0.0") //
+//				.description("Spring Boot REST API desc...") //
+//				.build();
+//	}
+//
+//	private SecurityContext securityContext() {
+//		return SecurityContext.builder().securityReferences(defaultAuth()).build();
+//	}
+//
+//	private List<SecurityReference> defaultAuth() {
+//		AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+//		AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+//		authorizationScopes[0] = authorizationScope;
+//		return Arrays.asList(new SecurityReference("JWT", authorizationScopes));
+//	}
+//
+//	private ApiKey apiKey() {
+//		return new ApiKey("JWT", "Authorization", "header");
+//	}
+//}
