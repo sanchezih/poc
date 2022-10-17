4ffe7624786e46e037b8d7270fc2dd2eaa419a69
Ignacio Sanchez <sanchezih@gmail.com>
2022-10-17 14:14:41 -0300

    Update Application.java

--- poc-spring-security/01-basic-auth/poc-ss-basic-auth-02-webmvc-jdbc/src/main/java/com/github/sanchezih/Application.java
+++ poc-spring-security/01-basic-auth/poc-ss-basic-auth-02-webmvc-jdbc/src/main/java/com/github/sanchezih/Application.java
@@ -4,6 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+/**
+ * Ejercicio basado en
+ * https://www.codejava.net/frameworks/spring-boot/form-authentication-with-jdbc-and-mysql
+ * 
+ * @author ihsanch
+ *
+ */
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
 
