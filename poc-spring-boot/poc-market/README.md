48855e6ab43419d615224d4652bb58698de659b5
Ignacio Sanchez <sanchezih@gmail.com>
2024-07-24 23:47:00 -0300

    delete poc market

--- poc-spring-boot/poc-market/README.md
+++
@@ -1,3 +0,0 @@
-# poc-market
-
-Repositorio market.
--- poc-spring-boot/poc-market/market-v1/src/main/java/com/github/sanchezih/market/service/IProduct.java
+++
@@ -1,8 +0,0 @@
-package com.github.sanchezih.market.service;
-
-import com.github.sanchezih.market.domain.Product;
-
-public interface IProduct {
-
-	public Product get();
-}
