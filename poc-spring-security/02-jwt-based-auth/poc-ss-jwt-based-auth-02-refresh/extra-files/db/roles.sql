a480169e9192edfe80ef040dfc6fa8e563393ff7
Ignacio Sanchez <sanchezih@gmail.com>
2024-11-16 12:36:21 -0300

    update

--- poc-spring-security/02-jwt-based-auth/poc-ss-jwt-based-auth-02-refresh/extra-files/db/roles.sql
+++
@@ -1,6 +0,0 @@
-USE pocjwtrefresh;
-
-delete from roles;
-INSERT INTO roles(name) VALUES('ROLE_USER');
-INSERT INTO roles(name) VALUES('ROLE_MODERATOR');
-INSERT INTO roles(name) VALUES('ROLE_ADMIN');
